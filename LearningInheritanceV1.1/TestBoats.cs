b6cb6c3266f1d88a2204067fb732ecb98298c50e
Hajk <tunczorx@gmail.com>
2018-03-30 12:40:54 +0200

    beginnings of inheritance information

---
+++ LearningInheritanceV1.1/TestBoats.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LearningInheritanceV2
+{
+    class TestBoats
+    {
+        public static void Main() // we decide that our program will start from here. 
+        {
+            string xyz = "";
+            Boat b1 = new Boat();
+            Sailboat b2 = new Sailboat();
+            Rowboat b3 = new Rowboat();
+            b2.setLength(32);
+            xyz = b1.move();
+            xyz += b3.move();
+            xyz += b2.move();
+            System.Windows.Forms.MessageBox.Show(xyz); // we had to add reference for System.Windows.Forms
+        }
+    }
+}
